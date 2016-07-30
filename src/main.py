4d0a46820c944f9bb38da0d231dabe15c14514c5
Windyf <ducbinh1510@gmail.com>
2016-07-30 15:04:55 +0700

    a

--- src/main.py
+++ src/main.py
@@ -2,4 +2,4 @@ import mnist_data_loader
 import network
 training_data,validation_data,test_data=mnist_data_loader.create_data()
 net = network.Network([784,15,10])
-net.gradient_descent(training_data,30,10,3.0,test_data=test_data)
+net.gradient_descent(training_data,50,10,1.0,test_data=test_data)
