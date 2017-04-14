2cb74793df27a28c08a0b34fc3b3903c30b7344d
Windy <ducbinh1510@gmail.com>
2017-04-14 22:47:46 +0700

    update number of neurals

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import mnist_data_loader
 import network
 training_data,validation_data,test_data=mnist_data_loader.create_data()
-net = network.Network([784,15,10])
+net = network.Network([784,20,10])
 net.gradient_descent(training_data,50,10,1.0,test_data=test_data)
